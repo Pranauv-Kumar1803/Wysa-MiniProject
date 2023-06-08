955d48bd2d19dcaec5d92b04a2e4e411c192487d
unknown <pranauv1803@gmail.com>
2023-06-08 10:25:42 +0530

    minor changes 3

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json({limit: '50mb'}));
 app.use(cookieParser());
 app.use(cors({
-    origin: ['https://wysa-miniproject.onrender.com'],
+    origin: ['https://wysa-miniproject.onrender.com','35.160.120.126','44.233.151.27','34.211.200.85'],
     methods: ['POST', 'GET', 'HEAD', 'PUT', 'DELETE'],
     credentials: true
 }))
