f16376c0f68221a015ea46862360316681c99ced
unknown <pranauv1803@gmail.com>
2023-06-08 10:17:31 +0530

    small change

--- client/src/api/axiosConfig.js
+++ client/src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://Chatbot_miniProj-api.onrender.com/api",
+  baseURL: "https://chatbot-miniproj-api.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
